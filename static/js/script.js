3bcf71fe7cc3be035fcc98d53fb0fc41004e6620
phongsulee <phongsulee@outlook.com>
2024-01-27 23:41:45 +0700

    cmv-5

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('card-shopping').addEventListener('click', function() {
+      window.location.href = 'order.html';
+    });
+  });
