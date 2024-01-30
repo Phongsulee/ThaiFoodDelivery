39c4e1a98b50434fee7a0724aede8bf3e21eaa27
Replit user <>
2024-01-30 14:14:37 +0000

    cmv-7

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('card-shopping').addEventListener('click', function() {
-      window.location.href = 'order.html';
+  document.getElementById('card-shopping').addEventListener('click', function() {
+     window.location.href = 'order.html';
     });
   });
+
+function showAlert() {
+    alert("ขออภัยสินค้าหมด");
+}
